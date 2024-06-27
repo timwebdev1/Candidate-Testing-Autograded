aa9eda92790a393dcc2b301ae8494a85f27c4912
Timothy McDermott <tlmcdermott79@gmail.com>
2024-06-27 17:57:24 -0500

    completed ga1 task2

--- candidate-testing.js
+++ candidate-testing.js
@@ -12,16 +12,16 @@ let candidateAnswer = '';
 
 
 //TODO: Variables for Part 2
-let questions = ["Who was the first American in space? ",
-   "True or false: 5 kilometers == 5000 meters? ",
-    "(5 + 3)/2 * 10 = ? ",
-     "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
-      "What is the minimum crew size of the ISS? "];
+let questions = ["Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? "];
 let correctAnswers = [
-  'Sally Ride', 
-  'true', 
-  '40', 
-  'Trajectory', 
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
   '3'];
 let candidateAnswers = [];
 
@@ -45,14 +45,14 @@ function askQuestion() {
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-// if (candidateAnswers === true) {
-//   console.log()
-// } else {
-//   console.log("Sorry, that is not correct.")
-// };
-console.log(`You answered: ${candidateAnswers} and the correct answers are: ${correctAnswers}`);
-// console.log(`Your Answers: ${candidateAnswers}; Correct Answers: ${correctAnswers}
-//   `);
+  // if (candidateAnswers === true) {
+  //   console.log()
+  // } else {
+  //   console.log("Sorry, that is not correct.")
+  // };
+  console.log(`You answered: ${candidateAnswers} and the correct answers are: ${correctAnswers}`);
+  // console.log(`Your Answers: ${candidateAnswers}; Correct Answers: ${correctAnswers}
+  //   `);
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
@@ -64,7 +64,7 @@ console.log(`You answered: ${candidateAnswers} and the correct answers are: ${co
 function runProgram() {
   askForName();
   // TODO 1.1c: Greet candidate using their name //
-   console.log(`Hello, ${candidateName}! Welcome to the Minimum Viable Quiz page. `);
+  console.log(`Hello, ${candidateName}! Welcome to the Minimum Viable Quiz page. `);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
